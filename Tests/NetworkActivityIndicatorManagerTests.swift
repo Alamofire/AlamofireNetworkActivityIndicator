c45ceef9661ee2030825827d9f3c90e0b40587ce
Christian Noon <christian.noon@gmail.com>
2019-04-10 10:44:18 -0700

    Refactored activity counting to more robust request id tracking (#49)
    * Refactored activity counting to more robust request id tracking
    
    * Removed unnecessary parameter portion of selector signature
    
    * Removed unnecessary lock on state didSet implementation

--- Tests/NetworkActivityIndicatorManagerTests.swift
+++ Tests/NetworkActivityIndicatorManagerTests.swift
@@ -1,7 +1,7 @@
 //
 //  NetworkActivityIndicatorManagerTests.swift
 //
-//  Copyright (c) 2016-2018 Alamofire Software Foundation (http://alamofire.org/)
+//  Copyright (c) 2016 Alamofire Software Foundation (http://alamofire.org/)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -30,9 +30,9 @@ import XCTest
 class NetworkActivityIndicatorManagerTestCase: XCTestCase {
     let timeout = 10.0
 
-    // MARK: - Tests - Manual Activity Count Updates
+    // MARK: - Tests - Manual Request Tracking
 
-    func testThatManagerCanTurnOnAndOffIndicatorWhenManuallyIncrementingAndDecrementingActivityCount() {
+    func testThatManagerCanTurnOnAndOffIndicatorWhenManuallyControllingRequests() {
         // Given
         let manager = NetworkActivityIndicatorManager()
         manager.startDelay = 0.0
@@ -48,8 +48,8 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
 
         // When
-        manager.incrementActivityCount()
-        dispatchAfter(0.1) { manager.decrementActivityCount() }
+        manager.requestDidStart(requestID: "r1")
+        dispatchAfter(0.1) { manager.requestDidStop(requestID: "r1") }
         waitForExpectations(timeout: timeout, handler: nil)
 
         // Then
@@ -62,7 +62,7 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
     }
 
-    func testThatManagerCanTurnOnAndOffIndicatorWhenManuallyControllingActivityCountWithMultipleChanges() {
+    func testThatManagerCanTurnOnAndOffIndicatorWhenManuallyControllingRequestsWithMultipleChanges() {
         // Given
         let manager = NetworkActivityIndicatorManager()
         manager.startDelay = 0.0
@@ -78,10 +78,10 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
 
         // When
-        manager.incrementActivityCount()
-        dispatchAfter(0.05) { manager.incrementActivityCount() }
-        dispatchAfter(0.10) { manager.decrementActivityCount() }
-        dispatchAfter(0.15) { manager.decrementActivityCount() }
+        manager.requestDidStart(requestID: "r1")
+        dispatchAfter(0.05) { manager.requestDidStart(requestID: "r2") }
+        dispatchAfter(0.10) { manager.requestDidStop(requestID: "r1") }
+        dispatchAfter(0.15) { manager.requestDidStop(requestID: "r2") }
 
         waitForExpectations(timeout: timeout, handler: nil)
 
@@ -95,7 +95,7 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
     }
 
-    func testThatManagerAppliesStartDelayWhenManuallyControllingActivityCount() {
+    func testThatManagerAppliesStartDelayWhenManuallyControllingRequests() {
         // Given
         let manager = NetworkActivityIndicatorManager()
         manager.startDelay = 0.125
@@ -107,8 +107,8 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
 
         // When
-        manager.incrementActivityCount()
-        dispatchAfter(0.05) { manager.decrementActivityCount() }
+        manager.requestDidStart(requestID: "r1")
+        dispatchAfter(0.05) { manager.requestDidStop(requestID: "r1") }
 
         let expectation = self.expectation(description: "visibility should change twice")
         dispatchAfter(0.25) { expectation.fulfill() }
@@ -120,7 +120,7 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         XCTAssertTrue(visibilityStates.isEmpty)
     }
 
-    func testThatManagerAppliesFinishDelayWhenManuallyControllingActivityCount() {
+    func testThatManagerAppliesFinishDelayWhenManuallyControllingRequests() {
         // Given
         let manager = NetworkActivityIndicatorManager()
         manager.startDelay = 0.0
@@ -136,10 +136,10 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
 
         // When
-        manager.incrementActivityCount()
-        dispatchAfter(0.1) { manager.decrementActivityCount() }
-        dispatchAfter(0.2) { manager.incrementActivityCount() }
-        dispatchAfter(0.3) { manager.decrementActivityCount() }
+        manager.requestDidStart(requestID: "r1")
+        dispatchAfter(0.1) { manager.requestDidStop(requestID: "r1") }
+        dispatchAfter(0.2) { manager.requestDidStart(requestID: "r2") }
+        dispatchAfter(0.3) { manager.requestDidStop(requestID: "r2") }
 
         waitForExpectations(timeout: timeout, handler: nil)
 
@@ -167,8 +167,8 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
 
         // When
-        manager.incrementActivityCount()
-        dispatchAfter(0.1) { manager.decrementActivityCount() }
+        manager.requestDidStart(requestID: "r1")
+        dispatchAfter(0.1) { manager.requestDidStop(requestID: "r1") }
 
         let expectation = self.expectation(description: "visibility should change twice")
         dispatchAfter(0.2) { expectation.fulfill() }
@@ -180,7 +180,7 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         XCTAssertTrue(visibilityStates.isEmpty)
     }
 
-    func testThatManagerIgnoresDecrementingActivityCountWhenAlreadyZero() {
+    func testThatManagerIgnoresDuplicateRequestDidStartCalls() {
         // Given
         let manager = NetworkActivityIndicatorManager()
         manager.startDelay = 0.0
@@ -193,10 +193,43 @@ class NetworkActivityIndicatorManagerTestCase: XCTestCase {
         }
 
         // When
-        manager.incrementActivityCount()
-        dispatchAfter(0.10) { manager.decrementActivityCount() }
-        dispatchAfter(0.15) { manager.decrementActivityCount() }
-        dispatchAfter(0.20) { manager.decrementActivityCount() }
+        manager.requestDidStart(requestID: "r1")
+        dispatchAfter(0.10) { manager.requestDidStart(requestID: "r1") }
+        dispatchAfter(0.15) { manager.requestDidStart(requestID: "r1") }
+        dispatchAfter(0.20) { manager.requestDidStop(requestID: "r1") }
+
+        let expectation = self.expectation(description: "visibility should change twice")
+        dispatchAfter(0.25) { expectation.fulfill() }
+
+        waitForExpectations(timeout: timeout, handler: nil)
+
+        // Then
+        XCTAssertTrue(manager.isEnabled)
+        XCTAssertEqual(visibilityStates.count, 2)
+
+        if visibilityStates.count == 2 {
+            XCTAssertTrue(visibilityStates[0])
+            XCTAssertFalse(visibilityStates[1])
+        }
+    }
+
+    func testThatManagerIgnoresDuplicateRequestDidStopCalls() {
+        // Given
+        let manager = NetworkActivityIndicatorManager()
+        manager.startDelay = 0.0
+        manager.completionDelay = 0.0
+
+        var visibilityStates: [Bool] = []
+
+        manager.networkActivityIndicatorVisibilityChanged = { isVisible in
+            visibilityStates.append(isVisible)
+        }
+
+        // When
+        manager.requestDidStart(requestID: "r1")
+        dispatchAfter(0.10) { manager.requestDidStop(requestID: "r1") }
+        dispatchAfter(0.15) { manager.requestDidStop(requestID: "r1") }
+        dispatchAfter(0.20) { manager.requestDidStop(requestID: "r1") }
 
         let expectation = self.expectation(description: "visibility should change twice")
         dispatchAfter(0.25) { expectation.fulfill() }
