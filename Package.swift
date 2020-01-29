9e18695ca28447ad76eb3e53450c741445ad48d7
Jon Shier <jon@jonshier.com>
2020-01-28 22:24:56 -0500

    Move version requirement to 5.0.0-rc.3.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "AlamofireNetworkActivityIndicator", targets: ["AlamofireNetworkActivityIndicator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-beta.6")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.3")
     ],
     targets: [
         .target(
